e079de773bf2416a3d880be3be1521555a7f8634
djnaSoft <88729193+djnaSoft@users.noreply.github.com>
2022-08-30 09:54:53 +0100

    async/await in passport strategy

--- index.js
+++ index.js
@@ -14,10 +14,15 @@ app.use(express.json());
 
 configurePassportToAuthenticateTokens();
 
-// passport for authorisation and authentication
+// We will use passport to verify credentials
+// We add it to each route we want to protect
+// We use only jwt, but passport can work with many other credentials as well.
 app.use(passport.initialize());
 
+// no passport check until we log in
 app.use('/login', AuthenticationRouter  );
+
+// add passport in front of each router we want to protect
 app.use('/books', passport.authenticate('jwt', {session: false}), BookRouter  );
 
 // handle errors, log diagnostic, give user simple error message
@@ -26,24 +31,24 @@ app.use(function (err, req, res, next) {
   res.status(500).send('System unable to process request, please try later.')
 })
 
-
 app.listen(3000, () => console.log('\nBookish listening on port 3000'));
 
-
+// provide passport with a way to validate user
 function configurePassportToAuthenticateTokens() {
     // Ensure that there is a valid JSON Web Token
     const jwtOptions = {};
     jwtOptions.jwtFromRequest = passportJwt.ExtractJwt.fromHeader('x-access-token');
     jwtOptions.secretOrKey = secret;
     const userRepository = new UserRepository();
-    passport.use(new passportJwt.Strategy(jwtOptions, (decodedJwt, next) => {
-        userRepository.getUserByName(decodedJwt.username)
-            .then(user => {
-                next(null, user);
-            }).catch(e => {
-              console.log("Authentication error ", e);
-              next('authentication failure: ' + decodedJwt.username);
-            }
-              );
+    passport.use(new passportJwt.Strategy(jwtOptions, async (decodedJwt, next) => {
+        try {
+           let user = await userRepository.getUserByName(decodedJwt.username);
+           // first param null means no error
+           next(null, user);
+        } catch( e ){
+            console.log("Authentication error ", e);
+            // report error in first param
+            next('authentication failure: ' + decodedJwt.username);
+        }
     }));
 }
